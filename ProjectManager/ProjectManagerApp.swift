0139d49a786e7945d0e15aa2fd075de18e0e573d
Egg Long <eddres229@gmail.com>
2024-06-18 22:35:18 -0400

    Initial Commit

---
+++ ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Created by Eli Dresnin on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
